a61844d50430410fc4ff72a7a95c247e5d35cd5e
KIMCOYG <kjhkjh9753@naver.com>
2020-01-08 00:16:26 +0900

    delBtn right position

--- todo.js
+++ todo.js
@@ -30,8 +30,8 @@ function paintToDo(text){
     delBtn.innerText = "DEL";
     delBtn.addEventListener("click", deleteToDo);
     span.innerText = text;
-    li.appendChild(delBtn);
     li.appendChild(span);
+    li.appendChild(delBtn);
     li.id = newId;
     toDoList.appendChild(li);
     const toDoObj = {
