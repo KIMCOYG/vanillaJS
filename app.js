6b003ed22f41733638924f161c2cf747aa4b7968
KIMCOYG <kjhkjh9753@gmail.com>
2020-02-06 17:35:24 +0900

    Connect mysql

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
-require('dotenv').config();
 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+import express from "express";
+import mysql from "mysql";
+import dbconfig from "./config/database";
+import env from "dotenv";
+import bodyParser from "body-parser";
 
-const app = express();
+env.config();
+
+const connection = mysql.createConnection(dbconfig);
+
 const port = process.env.PORT || 5000;
+const app = express();
 
 const successAlert = () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ` + app.get("port"));
 }
 
 app.use(express.static('public'));
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-mongoose.Promise = global.Promise;
+app.set("port", port);
 
-mongoose.connect(process.env.MONGO_URL, {useMongoClient: true})
-    .then(() => console.log("Successfully connected to mongodb"))
-    .catch(e => console.error(e));
+app.get("/", function(req, res){
+    res.send("Root");
+});
 
-app.listen(port, successAlert);
+app.get("/userinfo", function(req, res){
+    connection.query("SELECT * from User_Info", function(err, rows){
+        if(err)
+            throw err;
+        
+        console.log("The solution is: ", rows);
+        res.send(rows);
+    });
+});
+
+app.listen(app.get("port"), successAlert);
---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: "27.96.134.65",
+    user: "root",
+    password: "18rhdqngkwk!",
+    port: "3306",
+    database: "todolistjh"
+}
